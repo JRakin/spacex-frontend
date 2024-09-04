7386d81d63b8fce68bdc0d303a5f3090d870e294
Juaid Rakin <juaidrakin1@gmail.com>
2024-09-05 02:15:55 +0600

    initial configuration

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
+
+<style scoped></style>
